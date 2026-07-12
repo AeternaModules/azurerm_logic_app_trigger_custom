15708c02c840f4d3bd5ab3a8386c062e188b3f9b
Daniel Allen <dan@aeternagroup.net>
2026-07-12 14:18:39 +0100

    Update to schema version v4.80.0

--- outputs.tf
+++ outputs.tf
@@ -1,21 +1,17 @@
-output "logic_app_trigger_customs" {
-  description = "All logic_app_trigger_custom resources"
-  value       = azurerm_logic_app_trigger_custom.logic_app_trigger_customs
-}
 output "logic_app_trigger_customs_body" {
-  description = "List of body values across all logic_app_trigger_customs"
-  value       = [for k, v in azurerm_logic_app_trigger_custom.logic_app_trigger_customs : v.body]
+  description = "Map of body values across all logic_app_trigger_customs, keyed the same as var.logic_app_trigger_customs"
+  value       = { for k, v in azurerm_logic_app_trigger_custom.logic_app_trigger_customs : k => v.body }
 }
 output "logic_app_trigger_customs_callback_url" {
-  description = "List of callback_url values across all logic_app_trigger_customs"
-  value       = [for k, v in azurerm_logic_app_trigger_custom.logic_app_trigger_customs : v.callback_url]
+  description = "Map of callback_url values across all logic_app_trigger_customs, keyed the same as var.logic_app_trigger_customs"
+  value       = { for k, v in azurerm_logic_app_trigger_custom.logic_app_trigger_customs : k => v.callback_url }
 }
 output "logic_app_trigger_customs_logic_app_id" {
-  description = "List of logic_app_id values across all logic_app_trigger_customs"
-  value       = [for k, v in azurerm_logic_app_trigger_custom.logic_app_trigger_customs : v.logic_app_id]
+  description = "Map of logic_app_id values across all logic_app_trigger_customs, keyed the same as var.logic_app_trigger_customs"
+  value       = { for k, v in azurerm_logic_app_trigger_custom.logic_app_trigger_customs : k => v.logic_app_id }
 }
 output "logic_app_trigger_customs_name" {
-  description = "List of name values across all logic_app_trigger_customs"
-  value       = [for k, v in azurerm_logic_app_trigger_custom.logic_app_trigger_customs : v.name]
+  description = "Map of name values across all logic_app_trigger_customs, keyed the same as var.logic_app_trigger_customs"
+  value       = { for k, v in azurerm_logic_app_trigger_custom.logic_app_trigger_customs : k => v.name }
 }
 
